f2ebc02c8766f946e8feefd328bee79c546098d1
peng <alulur@163.com>
2015-12-25 21:11:16 +0800

    drawable Animation

---
+++ app/src/main/java/cn/dev/animation/animation/DrawableActivity.java
@@ -0,0 +1,19 @@
+package cn.dev.animation.animation;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import cn.dev.animation.R;
+
+/**
+ * Created by air on 2015/12/25.
+ */
+public class DrawableActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.activity_drawable);
+    }
+}
